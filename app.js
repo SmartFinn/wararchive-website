2e073d283cc0365f6352edd3dd590625dbd4ab7f
Serhii Yeremenko <finalitik@gmail.com>
2024-08-20 20:29:03 +0300

    Minor refactoring

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log(`${Date()} - Script started`);
+console.log(Date(), '- Script started');
 
 const esriTileLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
     attribution: '&copy; Powered by <a href="https://www.esri.com/">Esri</a>',
@@ -25,29 +25,28 @@ const stadiaOSMDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_sm
 });
 
 const markerIcon = L.icon({
-    iconUrl: '/img/marker.png', // Replace with the path to your custom icon
+    iconUrl: '/img/marker.png',
     iconRetinaUrl: '/img/marker@2x.png',
-    iconSize: [32, 32], // Size of the icon [width, height]
-    iconAnchor: [16, 32], // Point of the icon which will correspond to marker's location
-    popupAnchor: [0, -32], // Point from which the popup should open relative to the iconAnchor
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
     shadowUrl: '/img/marker-shadow.png',
     shadowRetinaUrl: '/img/marker-shadow@2x.png',
     shadowSize: [32, 32],
 });
 
-function groupMakerSvgIcon(count, fontSize = "12px") {
-    return `<svg xmlns="http://www.w3.org/2000/svg" width="42" height="42">
-        <path style="stroke:#000;stroke-width:2;stroke-linejoin:round;opacity:.2" d="M22 4A14 14 0 0 0 8 18a14 14 0 0 0 3.984 9.732L22 39l10.016-11.268A14 14 0 0 0 36 18 14 14 0 0 0 22 4Z"/>
-        <path style="fill:#ef5350;stroke-width:2;stroke:#ef5350;stroke-linejoin:round" d="M21 3A14 14 0 0 0 7 17a14 14 0 0 0 3.984 9.732L21 38l10.016-11.268A14 14 0 0 0 35 17 14 14 0 0 0 21 3z"/>
-        <circle style="fill:#fafafa" cx="21" cy="17" r="11"/>
-        <text xml:space="preserve" style="font-size:${fontSize};text-align:center;text-anchor:middle" x="20.63" y="20.85">
-        <tspan x="20.63" y="20.85">${count}</tspan></text></svg>`
+const groupMakerSvgIcon = (clusterCount, fontSize = "12px") => {
+    return `
+        <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42">
+            <path style="stroke:#000;stroke-width:2;stroke-linejoin:round;opacity:.2" d="M22 4A14 14 0 0 0 8 18a14 14 0 0 0 3.984 9.732L22 39l10.016-11.268A14 14 0 0 0 36 18 14 14 0 0 0 22 4Z"/>
+            <path style="fill:#ef5350;stroke-width:2;stroke:#ef5350;stroke-linejoin:round" d="M21 3A14 14 0 0 0 7 17a14 14 0 0 0 3.984 9.732L21 38l10.016-11.268A14 14 0 0 0 35 17 14 14 0 0 0 21 3z"/>
+            <circle style="fill:#fafafa" cx="21" cy="17" r="11"/>
+            <text xml:space="preserve" style="font-size:${fontSize};text-align:center;text-anchor:middle" x="20.63" y="20.85"><tspan x="20.63" y="20.85">${clusterCount}</tspan></text>
+        </svg>
+        `;
 }
 
-// load geojson to js variable
-    // URL of the GeoJSON file
 const geojsonUrl = '/data/wararchive_2024-08-17.geojson';
-let geojsonData;
 
 const allMarkers = L.markerClusterGroup({
     maxClusterRadius: 20,
@@ -59,19 +58,13 @@ const allMarkers = L.markerClusterGroup({
     polygonOptions: {
         color: '#c62828',
     },
-    iconCreateFunction: function (cluster) {
+    iconCreateFunction: cluster => {
         const childCount = cluster.getChildCount();
-        let labelFontSize = '12px';
-
-        if (childCount > 999) {
-            labelFontSize = '9px';
-        } else if (childCount > 99) {
-            labelFontSize = '10px';
-        }
+        const fontSize = childCount > 999 ? '9px' : childCount > 99 ? '10px' : '12px';
 
         return new L.DivIcon({
-            html: groupMakerSvgIcon(childCount, labelFontSize),
-            className: 'marker-group-icon',
+            html: groupMakerSvgIcon(childCount, fontSize),
+            className: 'cluster-marker-icon',
             iconSize: new L.Point(42, 42),
             iconAnchor: [21, 42],
         });
@@ -83,9 +76,8 @@ const documentWrap = document.getElementById('wrap');
 fetch(geojsonUrl)
     .then(response => response.json())
     .then(data => {
-        // Add the GeoJSON layer to the map
-        // geojsonData = data;
-        console.log(`${Date()} - GeoJSON loaded`);
+        console.log(Date(), '- GeoJSON loaded');
+
         L.geoJSON(data, {
             pointToLayer: function (feature, latlng) {
                 return L.marker(latlng, { icon: markerIcon });
@@ -94,7 +86,7 @@ fetch(geojsonUrl)
                 if (feature.properties && feature.properties.post_url) {
                     layer.bindPopup(`
                         <strong>Дата:</strong> ${feature.properties.date}<br/>
-                        <strong>Підрозділ:</strong> ${feature.properties.unit}<br/></br>
+                        <strong>Підрозділ:</strong> ${feature.properties.unit}<br/><br/>
                         ${feature.properties.description}<br/><br/>
                         <a href="${feature.properties.post_url}" target="_blank">${feature.properties.post_url}</a>
                         `
@@ -108,7 +100,7 @@ fetch(geojsonUrl)
         // throw new Error('Error parsing GeoJSON');
     })
     .then(() => {
-        console.log(`${Date()} - GeoJSON parsed`);
+        console.log(Date(), '- GeoJSON parsed');
         documentWrap.classList.add('loaded');
     })
     .catch(error => {
@@ -127,7 +119,6 @@ const baseLayers = {
 
 const overlays = {
     "Назви міст": onlyLabelsOverlay,
-    // "Markers": geojsonData,
 };
 
 L.control.layers(baseLayers, overlays).addTo(map);
@@ -139,19 +130,17 @@ map.on("dragend", updateHashLocation);
 map.on("zoomend", updateHashLocation);
 
 function parseUrlHash() {
-    const urlHash = location.hash.substring(1);
+    const hashParams = location.hash.slice(1).split('&');
 
-    if (!urlHash) {
+    if (hashParams.length === 0) {
         return {};
     }
 
-    return urlHash
-        .split('&')
-        .reduce((res, item) => {
-            let [key, value] = item.split('=');
-            res[key] = value;
-            return res;
-        }, {})
+    return hashParams.reduce((acc, param) => {
+        const [key, value] = param.split('=');
+        acc[key] = value;
+        return acc;
+    }, {});
 }
 
 function updateHashLocation() {
