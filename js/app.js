e87f214dcb6270e0573ac4be5c1d5712ea8ef54e
Serhii Yeremenko <finalitik@gmail.com>
2024-11-18 12:25:25 +0200

    Replace PNG makrers icons with SVGs

--- js/app.js
+++ js/app.js
@@ -26,31 +26,38 @@ const cartoDbDark = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/
     ext: 'png'
 });
 
-const markerIcon = L.icon({
-    iconUrl: '/assets/marker.png',
-    iconRetinaUrl: '/assets/marker@2x.png',
+const svgMarkerIcon = L.divIcon({
+    html: `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+        <path style="opacity:.2;stroke:#000;stroke-width:2;stroke-linejoin:round" d="M17 4A10 10 0 0 0 7 14a10 10 0 0 0 2.475 6.58L17 31l7.525-10.42A10 10 0 0 0 27 14 10 10 0 0 0 17 4Z"/>
+        <path style="fill:#f44336;stroke:#f44336;stroke-width:2;stroke-linejoin:round" d="M16 3A10 10 0 0 0 6 13a10 10 0 0 0 2.4746 6.58L16 30l7.525-10.42A10 10 0 0 0 26 13 10 10 0 0 0 16 3Z"/>
+        <circle style="fill:#fafafa" cx="16" cy="13" r="8"/>
+        <path style="fill:#4f4f4f" d="M12 10s-1 .114-1 1v4s0 1 1 1h5s1 0 1-1v-4s0-1-1-1h-5zm6 3 3 3v-6l-3 3z"/>
+    </svg>
+    `,
     iconSize: [32, 32],
     iconAnchor: [16, 32],
     popupAnchor: [0, -32],
-    shadowUrl: '/assets/marker-shadow.png',
-    shadowRetinaUrl: '/assets/marker-shadow@2x.png',
-    shadowSize: [32, 32],
+    className: 'marker-svg-icon',
 });
 
-const pointerIcon = L.icon({
-    iconUrl: '/assets/pointer.png',
-    iconRetinaUrl: '/assets/pointer@2x.png',
+const svgPointerIcon = L.divIcon({
+    html: `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+        <path style="opacity:.2" d="M2 17a14 14 0 0 0 14 14 14 14 0 0 0 14-14h-2a12 12 0 0 1-12 12A12 12 0 0 1 4 17Z"/>
+        <path style="fill:#3698f4" d="M16 2A14 14 0 0 0 2 16a14 14 0 0 0 14 14 14 14 0 0 0 14-14A14 14 0 0 0 16 2zm0 2a12 12 0 0 1 12 12 12 12 0 0 1-12 12A12 12 0 0 1 4 16 12 12 0 0 1 16 4z"/>
+        <circle style="opacity:.2;fill:#3698f4" cx="16" cy="16" r="13"/>
+    </svg>
+    `,
     iconSize: [32, 32],
     iconAnchor: [16, 16],
     popupAnchor: [0, -16],
-    shadowUrl: '/assets/pointer-shadow.png',
-    shadowRetinaUrl: '/assets/pointer-shadow@2x.png',
-    shadowSize: [32, 32],
+    className: 'pointer-svg-icon',
 });
 
 const createClusterIcon = (clusterCount, fontSize = "12px") => {
     return `
-        <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42">
             <path style="stroke:#000;stroke-width:2;stroke-linejoin:round;opacity:.2" d="M22 4A14 14 0 0 0 8 18a14 14 0 0 0 3.984 9.732L22 39l10.016-11.268A14 14 0 0 0 36 18 14 14 0 0 0 22 4Z"/>
             <path style="fill:#ef5350;stroke-width:2;stroke:#ef5350;stroke-linejoin:round" d="M21 3A14 14 0 0 0 7 17a14 14 0 0 0 3.984 9.732L21 38l10.016-11.268A14 14 0 0 0 35 17 14 14 0 0 0 21 3z"/>
             <circle style="fill:#fafafa" cx="21" cy="17" r="11"/>
@@ -90,7 +97,7 @@ fetch(geojsonUrl)
     .then(response => response.json())
     .then(data => {
         L.geoJSON(data, {
-            pointToLayer: (feature, latlng) => L.marker(latlng, { icon: markerIcon }),
+            pointToLayer: (feature, latlng) => L.marker(latlng, { icon: svgMarkerIcon }),
             onEachFeature: (feature, layer) => {
                 if (!feature.properties) {
                     throw new Error('GeoJSON feature has no properties');
@@ -164,7 +171,7 @@ function setViewFromHashLocation() {
     }
     if (urlParam.goto) {
         const [lat, lng] = urlParam.goto.split(',');
-        const gotoMarker = L.marker([lat, lng], { icon: pointerIcon }).addTo(map);
+        const gotoMarker = L.marker([lat, lng], { icon: svgPointerIcon }).addTo(map);
         gotoMarker.bindPopup(`<code>${lat}, ${lng}</code>`);
         map.setView([lat, lng], 16); // Set a default zoom level for goto
     }
